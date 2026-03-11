17fcc3b19f9735dd90ba17816fddb3423ad8b3ac
quan <daomquan2004@gmail.com>
2026-03-11 13:27:29 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Cho phép HMR/WebSocket khi truy cập qua IP – server phải chạy với -H 0.0.0.0
   allowedDevOrigins: [
     "http://localhost:3000",
+    "http://localhost:3000/",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:3000/",
     "http://172.29.48.1:3000",
+    "http://172.29.48.1:3000/",
+    // IP khác trong mạng nội bộ (đổi theo IP máy bạn)
+    "http://[::1]:3000",
+    "http://[::1]:3000/",
   ],
 };
 
